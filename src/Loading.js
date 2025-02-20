259b32a6405376bd8ff6ca16cbad3567cc9702f3
Mohit <145916003+imohitpatel@users.noreply.github.com>
2025-02-20 22:09:39 +0530

    loading message

---
+++ src/Loading.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Loading = () => {
+	return (
+		<>
+			<div className="loadingPage">
+				<div className="loadingPop">
+					<h1 className="loading">Calculating your travel itinerary mera safar...</h1>
+					<h3 className="loading">Please be patient, long routes can take up to one minute to calculate</h3>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Loading;
