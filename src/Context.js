82b605c5b4cd0ed05d4129358bdcb787e4e781f6
Mohit <145916003+imohitpatel@users.noreply.github.com>
2025-02-20 22:00:55 +0530

    main look and feel changed ,layout color button etc

---
+++ src/Context.js
@@ -0,0 +1,23 @@
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+const baseURL =
+  process.env.REACT_APP_MODE === "prod"
+    ? "https://itinerary-generator-node.nw.r.appspot.com/"
+    : "http://localhost:8080/";
+
+export const myContext = createContext({});
+export default function Context(props) {
+  const [userObject, setUserObject] = useState({});
+  console.log(userObject)
+  useEffect(() => {
+    axios.get(baseURL + "getuser", { withCredentials: true }).then((res) => {
+      if (res.data) {
+        setUserObject(res.data);
+      }
+    });
+  }, []);
+  return (
+    <myContext.Provider value={userObject}>{props.children}</myContext.Provider>
+  );
+}
