c290e02bfbe72abf312200ac6740fd0acceaea88
Mohit <145916003+imohitpatel@users.noreply.github.com>
2025-02-20 22:04:59 +0530

    given copyright or resource mentions

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer>
+			<div>Icon made by <a href="https://www.freepik.com" target="_blank" rel="noreferrer" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" target="_blank" rel="noreferrer" title="Flaticon">www.flaticon.com</a></div>
+			<div>Directions from <a href="https://openrouteservice.org/" target="_blank" rel="noreferrer" title="OpenRouteService">OpenRouteService</a></div>
+			<div>Places information from <a href="https://opentripmap.io/" target="_blank" rel="noreferrer" title="OpenTripMap">OpenTripMap</a></div>
+			<div><span>Background photo by <a href="https://www.pexels.com/@element5">Element5 Digital</a> on <a href="https://www.pexels.com/">Pexels</a></span></div>
+		</footer>
+	);
+}
+
+export default Footer;
