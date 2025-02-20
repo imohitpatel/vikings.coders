933933c47927ca8365ce991b80e45268c1e5cddc
Mohit <145916003+imohitpatel@users.noreply.github.com>
2025-02-20 22:07:15 +0530

    add loading message

---
+++ src/Latest.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import SavedTrip from "./SavedTrip";
+
+const baseURL =
+  process.env.REACT_APP_MODE === "prod"
+    ? "https://-----.nw.r.appspot.com/" //add name
+    : "http://localhost:8080/";
+
+const Latest = () => {
+  const [latestTrips, setLatestTrips] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    fetch(baseURL + "trips")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          alert("Could not load latest trips");
+        } else {
+          setLatestTrips(data);
+          setIsLoaded(true);
+        }
+      });
+  }, []);
+  return (
+    <section className="tripContainer">
+      {!isLoaded && (
+        <div className="margin">Loading latest trips, please wait...</div>
+      )}
+      {isLoaded &&
+        latestTrips.map((el) => (
+          <SavedTrip
+            {...el}
+            isPublic={el.public}
+            poiDetails1={el.poiDetails}
+            key={el._id}
+          />
+        ))}
+    </section>
+  );
+};
+
+export default Latest;
