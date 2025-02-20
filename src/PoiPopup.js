361bca7d631f067acb9bcb4faada73db66251c67
Mohit <145916003+imohitpatel@users.noreply.github.com>
2025-02-20 22:23:22 +0530

    moved map zoomand add save your trip button

---
+++ src/PoiPopup.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const PoiPopup = ({ poi, updateTrip }) => {
+  const wikipedia_extracts = poi.wikipedia_extracts
+    ? poi.wikipedia_extracts.html
+    : "No description available";
+  return (
+    <>
+      <h4>{poi.name}</h4>
+      <img className="popupImg" alt={poi.name} src={poi.preview.source}></img>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: wikipedia_extracts,
+        }}
+      ></div>
+      <a href={poi.wikipedia} target="_blank" rel="noreferrer">
+        See more on Wikipedia
+      </a>
+      {/* <button onClick={() => updateTrip(poi.xid)}>
+        {poi.excluded ? "Include in travel itinerary" : "Exclude from itinerary"}
+      </button> */}
+    </>
+  );
+};
+
+export default PoiPopup;
